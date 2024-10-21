27a8cb39ea3a7951f84baff79301c95e5a146c55
mamoraw <144896087+mamoraw@users.noreply.github.com>
2024-10-21 13:19:38 +0200

    added first table

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
